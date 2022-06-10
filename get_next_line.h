26d5a8f7a6a1c0ae996369d8b415bf66b40ff07a
Guillermo <104837331+g-romero-almansa@users.noreply.github.com>
2022-06-10 11:38:14 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gromero- <gromero-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 12:05:31 by gromero-          #+#    #+#             */
+/*   Updated: 2022/05/05 11:50:48 by gromero-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t			ft_strlen(char *str);
+
+char			*ft_strchr(char *s, int c);
+
+char			*ft_strjoin(char *s1, char *s2);
+
+char			*get_next_line(int fd);
+
+char			*ft_get_line(char *save);
+
+char			*ft_save(char *save);
+
+char			*ft_read_and_save(int fd, char *save);
+
+#endif
